2882bb80b3491e58a53e452c152477620481e11f
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2022-08-30 08:05:03 -0300

    add custom response header

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, HttpCode, Post, Req } from '@nestjs/common';
+import { Controller, Get, Header, HttpCode, Post, Req } from '@nestjs/common';
 import { Request } from "express";
 
 @Controller('cats')
 export class CatsController {
     @Post()
     @HttpCode(204)
+    @Header('Cache-Control', 'none')
     create(): string {
         return 'Add a new cat'
     }
