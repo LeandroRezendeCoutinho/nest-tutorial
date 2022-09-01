88d521704e6b4b34b03f4696bfd35027007609c0
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2022-09-01 08:35:29 -0300

    make core module global, so any module can inject exported providers by the core without import it

--- src/core.module.ts
+++ src/core.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { CommonModule } from './common.module';
 
+@Global()
 @Module({
     imports: [CommonModule],
 })
