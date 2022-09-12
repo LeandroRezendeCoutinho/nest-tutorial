ecaf8ab4516e52f5272b1a03dbfb03bd77bb066f
leandrorezendecoutinho <eleandro.rc.contato@gmail.com>
2022-09-12 08:38:03 -0300

    create a validation pipe

---
+++ src/validations/validation.pipe.ts
@@ -0,0 +1,11 @@
+import { ArgumentMetadata, Injectable, PipeTransform } from "@nestjs/common"
+
+
+@Injectable()
+export class ValidationPipe implements PipeTransform {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    transform(value: any, metadata: ArgumentMetadata) {
+        return value
+    }
+
+}
