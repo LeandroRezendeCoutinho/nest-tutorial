701670beba094bd30afc01e016b5fc2643970f86
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2022-09-19 09:46:10 -0300

    use validation pipe globally by DI

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,23 @@ import {
   NestModule,
   RequestMethod,
 } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { logger } from './middlewares/logger.middleware';
+import { ValidationPipe } from './validations/validation.pipe';
 
 @Module({
   imports: [CatsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe
+    }
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
