5cf184155c2af4fa4f7041eea0c8ed58d1a5f952
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2022-10-07 08:42:42 -0300

    determine a provider dinamically

--- src/common.module.ts
+++ src/common.module.ts
@@ -1,8 +1,18 @@
 import { Module } from "@nestjs/common"
-import { CatsModule } from "./cats/cats.module"
+import { ConfigService } from "./config/config.service"
+import { DevelopmentConfigService } from "./config/development-config.service"
+import { ProductionConfigService } from "./config/production-config.service"
+
+const configServiceProvider = {
+  provide: ConfigService,
+  useClass:
+    process.env.NODE_ENV === 'development'
+      ? DevelopmentConfigService
+      : ProductionConfigService
+}
 
 @Module({
-    imports: [CatsModule],
-    exports: [CatsModule]
+    providers: [configServiceProvider]
 })
+  
 export class CommonModule {}
