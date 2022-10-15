4710a84f24580f7367f561eb006d7d5b6d23716d
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2022-10-15 19:38:33 -0300

    clusterize application

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,35 @@
 import { NestFactory } from '@nestjs/core'
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify'
 import { AppModule } from './app.module'
-import { LoggingInterceptor } from './interceptors/logging.interceptor'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  app.useGlobalInterceptors(new LoggingInterceptor())
-  await app.listen(3000)
+import cluster from 'node:cluster'
+import { cpus } from 'os'
+import { Logger } from '@nestjs/common'
+
+if (cluster.isPrimary) {
+  const cpuCount = cpus().length
+
+  for (let i = 0; i < cpuCount; i += 1) {
+    cluster.fork()
+  }
+
+  cluster.on('online', worker => {
+    Logger.log('Worker ' + worker.process.pid + ' is online.')
+  })
+  cluster.on('exit', ({ process }) => {
+    Logger.log('worker ' + process.pid + ' died.')
+  })
+} else {
+
+  async function bootstrap() {
+    const app = await NestFactory.create<NestFastifyApplication>(
+      AppModule,
+      new FastifyAdapter()
+    )
+    await app.listen(3000)
+  }
+  bootstrap()
 }
-bootstrap()
