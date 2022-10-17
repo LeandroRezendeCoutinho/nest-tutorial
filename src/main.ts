046825232fc363cd35ba13d1e597accb08a25bb8
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2022-10-17 08:14:26 -0300

    disable cluster

--- src/main.ts
+++ src/main.ts
@@ -5,24 +5,24 @@ import {
 } from '@nestjs/platform-fastify'
 import { AppModule } from './app.module'
 
-import cluster from 'node:cluster'
-import { cpus } from 'os'
-import { Logger } from '@nestjs/common'
+// import cluster from 'node:cluster'
+// import { cpus } from 'os'
+// import { Logger } from '@nestjs/common'
 
-if (cluster.isPrimary) {
-  const cpuCount = cpus().length
+// if (cluster.isPrimary) {
+//   const cpuCount = cpus().length
 
-  for (let i = 0; i < cpuCount; i += 1) {
-    cluster.fork()
-  }
+//   for (let i = 0; i < cpuCount; i += 1) {
+//     cluster.fork()
+//   }
 
-  cluster.on('online', worker => {
-    Logger.log('Worker ' + worker.process.pid + ' is online.')
-  })
-  cluster.on('exit', ({ process }) => {
-    Logger.log('worker ' + process.pid + ' died.')
-  })
-} else {
+//   cluster.on('online', worker => {
+//     Logger.log('Worker ' + worker.process.pid + ' is online.')
+//   })
+//   cluster.on('exit', ({ process }) => {
+//     Logger.log('worker ' + process.pid + ' died.')
+//   })
+// } else {
 
   async function bootstrap() {
     const app = await NestFactory.create<NestFastifyApplication>(
@@ -32,4 +32,4 @@ if (cluster.isPrimary) {
     await app.listen(3000)
   }
   bootstrap()
-}
+// }
