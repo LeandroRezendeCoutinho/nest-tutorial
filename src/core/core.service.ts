08ee15477691f32469a15b14f4998bb46180bd42
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2022-11-03 07:48:17 -0300

    dynamically resolve a scoped provider

--- src/core/core.service.ts
+++ src/core/core.service.ts
@@ -1,3 +1,13 @@
-export default class CoreService {
-    
+import { Injectable, OnModuleInit } from "@nestjs/common"
+import { ModuleRef } from "@nestjs/core"
+import { TransientService } from "./transient.service"
+
+@Injectable()
+export default class CoreService implements OnModuleInit {
+    private transientService: TransientService
+    constructor(private moduleRef: ModuleRef) { }
+
+    async onModuleInit() {
+        this.transientService = await this.moduleRef.resolve(TransientService)
+    }
 }
