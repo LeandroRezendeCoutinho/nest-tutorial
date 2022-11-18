3d0eea05c7e3412120dd50fdc8aa8c33c8068b34
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2022-11-18 07:47:34 -0300

    fix user controller test

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { User } from './entities/user.entity'
 import { UserController } from './user.controller'
 import { UserService } from './user.service'
 
@@ -8,7 +10,15 @@ describe('UserController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: {
+            find: jest.fn().mockResolvedValue({})
+          }
+        }
+      ],
     }).compile()
 
     controller = module.get<UserController>(UserController)
